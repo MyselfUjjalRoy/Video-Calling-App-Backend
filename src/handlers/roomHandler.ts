e8da37c961f11e44023f0a7bda7ef63e87611856
Ujjal Roy <royu99099@gmail.com>
2025-06-26 17:26:14 +0530

    Room creation logic with user joining the room and there record of joining being stored added

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,17 +1,41 @@
 import { Socket } from "socket.io";
 import { v4 as UUIDv4 } from "uuid";
+import IRoomParams from "../config/IRoomParams";
+
+const rooms: Record<string , string[]> = {};
 
 const roomHandler = (socket: Socket) => {
+
+  // the below map stores for a room what all peers have joined
+  
 
   const createRoom = () => {
     const roomId = UUIDv4(); // this will be our unique room id in which multiple connection will exchange
     socket.join(roomId); // we will make the socket connection enter a new room
+
+    rooms[roomId] = []; // create a new entry for the room
+
     socket.emit("room-created", { roomId }); // we will emit an event from server side that socket co
     console.log("Room created with id ", roomId);
   };
 
-  const joinedRoom = ({roomId} : {roomId: string}) => {
-    console.log("New user has joined room",roomId);
+  const joinedRoom = ({roomId , peerId}: IRoomParams ) => {
+    console.log("joined room called", rooms,roomId, peerId);
+    if (rooms[roomId]) {
+      // If the given roomId exists in the in memory db
+      console.log("New user has joined room", roomId, "with peer id as", peerId);
+      // the moment new user joins, add the peerId to the key of roomId
+      rooms[roomId].push(peerId);
+      console.log("added peer to room", rooms);
+      socket.join(roomId); // make the user join the socket room
+
+      // below event is for logging purpose
+      socket.emit("get-users", {
+        roomId,
+        participants: rooms[roomId],
+      });
+
+    }
   };
 
   // When to call the above two function ?
